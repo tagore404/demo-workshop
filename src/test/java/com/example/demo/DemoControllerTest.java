bb8871da6af75a5a8aece032a529510e58f1ec97
tagore404 <tagorech11@gmail.com>
2025-06-24 03:51:49 -0700

    Update DemoControllerTest.java

--- src/test/java/com/example/demo/DemoControllerTest.java
+++ src/test/java/com/example/demo/DemoControllerTest.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DemoControllerTest {
+public class AppTest {
 
     @Test
-    public void testHello() {
-        DemoController controller = new DemoController();
-        assertEquals("Hello from Demo!", controller.hello());
+    public void testSayHello() {
+        App app = new App();
+        assertEquals("Hello Jenkins!", app.sayHello());
     }
 }
